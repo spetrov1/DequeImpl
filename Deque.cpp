f1b0d8c026b0d8f006974e1b1b557c3adfe55219
spetrov1 <petrov165@abv.bg>
2020-03-10 21:51:23 +0200

    Working addFirst(), constructor, .gitignore file ...

---
+++ Deque.cpp
@@ -0,0 +1,80 @@
+// Deque.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cassert>
+#include "Deque.h"
+
+// tests
+/*
+bool addFirstToDequeInitializedWithOneCapacity() {
+    Deque<int> d(1);
+    d.addFirst(1);
+
+    // TODO
+}
+
+bool addFirstMultipleTimesDequeInitOneCapacity() {
+    Deque<int> d(1);
+    d.addFirst(1);
+    d.print();
+
+    d.addFirst(2);
+    d.print();
+
+    d.addFirst(15);
+    d.print();
+
+    // TODO
+}
+
+bool addFirstDequeNotAllocatedMemory() { // Initialized with default constructor
+    Deque<int> deq;
+    deq.addFirst(1);
+    deq.print();
+
+    // TODO
+}
+
+bool addFirstMultipleTimesDequeInitializedNotAllocatedMemory() {
+    Deque<int> deq;
+    deq.addFirst(1);
+    deq.print();
+
+    deq.addFirst(2);
+    deq.print();
+
+    deq.addFirst(3);
+    deq.print();
+
+    // TODO
+}
+
+*/
+
+int main()
+{   
+
+    Deque<int> deq(4);
+    deq.addFirst(1);
+    deq.print();
+    
+    deq.addFirst(2);
+    deq.print();
+
+    deq.addFirst(3);
+    deq.addFirst(4);
+
+    deq.print();
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ Deque.h
@@ -0,0 +1,117 @@
+#pragma once
+
+template <class T>
+class Deque
+{
+private:
+	size_t capacity = 0;
+	size_t size = 0;
+	size_t headIndex = 0;
+	T* buffer = nullptr;
+
+	const short resizeCoeff = 2;
+
+	void resize();
+public:
+	Deque(size_t capacity = 0);
+
+	void addFirst(T newElem);
+
+	void print() const {
+		if (isEmpty()) 
+			return;
+
+		size_t tailIndex = getTailIndex();
+
+		for (size_t i = headIndex; i <= tailIndex ; ++i) {
+			std::cout << buffer[i] << " ";
+		} 
+		std::cout << "some meta data ... headIndex = " << headIndex <<
+			" ,size = " << size << " ,capacity = " << capacity << std::endl;
+	}
+
+	size_t getTailIndex() const;
+	bool isEmpty() const;
+	bool notAllocatedMemory() const;
+	void allocateSomeMemory();
+};
+
+template <typename T>
+Deque<T>::Deque(size_t capacity) {
+	if (capacity == 0) 
+		return;
+	buffer = new T[capacity]; // may throw bad_aloc
+	this->capacity = capacity;
+	headIndex = capacity / 2;
+}
+
+template <typename T>
+void Deque<T>::addFirst(T newElem) {
+	assert(newElem);
+
+	if (notAllocatedMemory()) {
+		allocateSomeMemory();
+	}
+	else if (isEmpty()) { // empty && allocatedSomeMemoryYet
+		headIndex = capacity / 2; // TODO Head is expected to be on right position, so it is not needed maybe 
+	}
+	else if (!isEmpty()) {
+		if (headIndex == 0) {
+			resize();
+		}
+		--headIndex;     // We have head element and we skip it
+						 // Prepare for the new head
+	}
+	buffer[headIndex] = newElem;
+	++size;
+}
+
+template <typename T>
+void Deque<T>::resize() {
+	int newCapacity = resizeCoeff * size;
+	int offset = size / 2;
+	int newHeadIndex = offset;
+	T* temp = buffer;
+
+	if (size == 1) {
+		newCapacity = 3; // TODO name constant 3
+		offset = newCapacity / 2;
+		newHeadIndex = offset;
+	}
+
+	// Try to isolate the assigning in other function
+	buffer = new T[newCapacity]; // TODO may throw bad_alloc
+	for (int i = 0; i < size; ++i) {
+		buffer[newHeadIndex + i] = temp[headIndex + i];
+	}
+
+	headIndex = newHeadIndex;
+	capacity = newCapacity;
+}
+
+template <typename T>
+size_t Deque<T>::getTailIndex() const {
+	assert(size > 0); // or throw exception ?
+
+	return headIndex + size - 1;
+}
+
+template <typename T>
+bool Deque<T>::isEmpty() const {
+	return size == 0;
+}
+
+template <typename T>
+bool Deque<T>::notAllocatedMemory() const {
+	return !buffer; // && capacity == 0 && size == 0 && headIndex == 0
+}
+
+template <typename T>
+void Deque<T>::allocateSomeMemory() {
+	const short defaultMemoryAllocation = 3;
+	buffer = new T[defaultMemoryAllocation]; // may throw bad_aloc exc ?
+
+	capacity = defaultMemoryAllocation;
+	headIndex = capacity / 2;
+	size = 0; // Not needed maybe. It is expected to be this value
+}
